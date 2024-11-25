aaf88e017fcf7369ec11fe6f6b29986e4bc86685
EasoncoderTW <easonyehmobile1@gmail.com>
2024-11-25 22:58:30 +0800

    firt release

--- build_src.bat
+++
@@ -1,4 +0,0 @@
-@REM compile lib
-for /f %%f in ('dir /b .\customLIB\source') do gcc -shared -c ./customLIB/source/%%f -o ./customLIB/obj/%%~nf.o -IcustomLIB/include
-
-for /f %%f in ('dir /b .\customLIB\obj') do ar rcs ./customLIB/lib/lib%%~nf.a ./customLIB/obj/%%f
